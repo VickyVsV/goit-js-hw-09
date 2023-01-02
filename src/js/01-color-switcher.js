7390efe2e467719dfbb08c8955fa18b8aa67e7eb
VickyVsV <vikusiavsv@gmail.com>
2023-01-03 01:43:42 +0200

    done 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]')
+};
 
+let timerId = null;
+
+refs.btnStart.addEventListener('click', onBtnStart);
+refs.btnStop.addEventListener('click', onBtnStop);
+refs.btnStop.disabled = true;
+
+function onBtnStart(){
+  //console.log(refs.btnStart);
+  timerId = setInterval(() => {
+    onChangeBodyColor()
+  }, 1000);
+  refs.btnStart.disabled = true;
+  refs.btnStop.disabled = false;
+}
+
+function onBtnStop(){
+  //console.log(refs.btnStop);
+  clearInterval(timerId);
+  refs.btnStart.disabled = false;
+
+}
+
+function onChangeBodyColor(){
+  //console.log(refs.btnStart);
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
